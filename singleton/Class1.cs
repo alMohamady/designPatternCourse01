a1417fa74e795a9ea0b9f8d9936ca6b92c04a379
al Mohamady <al.Mohamady47@gmail.com>
2020-09-27 19:05:19 +0200

    inith commit

---
+++ singleton/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    sealed class Class1
+    {
+        private static int counter;
+        private static readonly Lazy<Class1> instance = new Lazy<Class1>(() => new Class1());
+
+        private Class1()
+        {
+            counter += 1;
+            Console.WriteLine(" Instance Number  " + counter.ToString());
+        }
+
+        public static Class1 getInstance()
+        {
+            return instance.Value;
+        }
+
+        public void print(string msg)
+        {
+            Console.WriteLine(msg);
+        }    
+    }
+}
---
+++ singleton/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => method1(),
+                () => method2()
+                );
+
+            Console.ReadKey(true);
+
+        }
+
+        public static void method1()
+        {
+            Class1 c = Class1.getInstance();
+            c.print("Hello world");
+        }
+
+        public static void method2()
+        {
+            Class1 c2 = Class1.getInstance();
+            c2.print("adsadasd");
+            c2.print("adsadasd");
+        }
+    }
+}
