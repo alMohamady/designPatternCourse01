f6620dccccd07dbd18053e8407e58811ae6cdae9
al Mohamady <al.Mohamady47@gmail.com>
2020-09-30 03:23:51 +0200

    Prototype design pattern

--- singleton/Program.cs
+++ singleton/Program.cs
@@ -5,23 +5,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using singleton.Builder;
-
+using singleton.Prototype;
 
 namespace singleton
 {
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
-            IRobotBuilder robotBuilderOld = new RobotBulderOld();
-            
-            RobotBuilder robotBuilder = new RobotBuilder(robotBuilderOld);
-            robotBuilder.makeRobot();
 
-            Robot robot = robotBuilder.getRobot();
+            Person p1 = new Person();
+            p1.Age = 42;
+            p1.BirthDate = Convert.ToDateTime("1977-01-01");
+            p1.Name = "Ahmed Mohamady";
+            p1.idInfo = new IdInfo(666);
 
-            Console.WriteLine("  Robot Head " + robot.getRobotHead());
+            Person p2 = p1.ShallowCopy();
+            Person p3 = p1.DeepCopy();
+            Person p4 = p1;
+
+            Console.WriteLine("Original values of p1, p2, p3, p4:");
+            Console.WriteLine("   p1 instance values: ");
+            DisplayValues(p1);
+            Console.WriteLine("   p2 instance values:");
+            DisplayValues(p2);
+            Console.WriteLine("   p3 instance values:");
+            DisplayValues(p3);
+            Console.WriteLine("   p4 instance values:");
+            DisplayValues(p4);
+
+            p1.Age = 32;
+            p1.BirthDate = Convert.ToDateTime("1900-01-01");
+            p1.Name = "Mohamed Salah";
+            p1.idInfo.IdNumber = 7878;
+
+            Console.WriteLine("\nValues of p1, p2, p4 and p3 after changes to p1:");
+            Console.WriteLine("   p1 instance values: ");
+            DisplayValues(p1);
+            Console.WriteLine("   p2 instance values (reference values have changed):");
+            DisplayValues(p2);
+            Console.WriteLine("   p3 instance values (everything was kept the same):");
+            DisplayValues(p3);
+            Console.WriteLine("   p4 instance values");
+            DisplayValues(p4);
+
             Console.ReadKey(true);
         }
+
+        public static void DisplayValues(Person p)
+        {
+            Console.WriteLine("      Name: {0:s}, Age: {1:d}, BirthDate: {2:MM/dd/yy}",
+                p.Name, p.Age, p.BirthDate);
+            Console.WriteLine("      ID#: {0:d}", p.idInfo.IdNumber);
+        }
+
+
     }
 }
---
+++ singleton/Prototype/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton.Prototype
+{
+    class Person
+    {
+        public int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public IdInfo idInfo;
+
+        public Person ShallowCopy()
+        {
+            return (Person)this.MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        { 
+           Person person = (Person)this.MemberwiseClone();
+           person.idInfo = new IdInfo(this.idInfo.IdNumber);
+            person.Name = String.Copy(Name);
+            return person;
+        }
+
+    }
+
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+}
