0aff3f4cbad68cb9894a421534a6abf1f0c0b110
al Mohamady <al.Mohamady47@gmail.com>
2020-10-03 00:46:32 +0200

    Adapter pattern

--- singleton/Program.cs
+++ singleton/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using singleton.Builder;
 using singleton.Prototype;
+using singleton.Adapter;
 
 namespace singleton
 {
@@ -17,39 +18,11 @@ namespace singleton
         static void Main(string[] args)
         {
 
-            Person emp1 = new Person()
-            {
-                Id = Guid.NewGuid(),
-                Name = "Ahmed",
-                Age = 35,
-                AddressDetails = new Adress()
-                {
-                    DoorNumber = 10,
-                    StreetNumber = 20,
-                    Zipcode = 90025,
-                    Country = "US"
-                }
-            };
 
-            Console.WriteLine(emp1.ToString());
+            IEmployee emp = new EmployeeAdapter();
+            string value = emp.GetAllEmployees();
 
-            Person emp2 = emp1.DeepCopy();
-
-            emp2.Name = "Mohmed";
-            emp2.Age = 50;
-            emp2.AddressDetails.StreetNumber = 21;
-            emp2.AddressDetails.DoorNumber = 11;
-            
-            Console.WriteLine(emp2.ToString());
-
-            Console.WriteLine("Modified Details of Ahmed");
-            emp1.AddressDetails.DoorNumber = 30;
-            emp1.AddressDetails.StreetNumber = 40;
-
-            emp1.Age = 40;
-            Console.WriteLine(emp1.ToString());
-            Console.WriteLine(emp2.ToString());
-            Console.ReadLine();
+            Console.WriteLine(value);  
 
             Console.ReadKey(true);
         }
