db7aa1121e424d3c6d606f9ebf38572c45a3ef3f
al Mohamady <al.Mohamady47@gmail.com>
2020-10-09 18:02:03 +0200

    Facade pattern

--- singleton/Program.cs
+++ singleton/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using ShoppingCartFacade;
 using singleton.Flyweight;
 using System;
 using System.Collections.Generic;
@@ -16,51 +17,20 @@ namespace singleton
         static void Main(string[] args)
         {
 
-            var factory = new FlyweightFactory(
-                new Car { Company = "Chevrolet", Model = "Camaro2018", Color = "pink" },
-                new Car { Company = "Mercedes Benz", Model = "C300", Color = "black" },
-                new Car { Company = "Mercedes Benz", Model = "C500", Color = "red" },
-                new Car { Company = "BMW", Model = "M5", Color = "red" },
-                new Car { Company = "BMW", Model = "X6", Color = "white" }
-            );
-            factory.listFlyweights();
-
-            addCarToPoliceDatabase(factory, new Car
-            {
-                Number = "CL234IR",
-                Owner = "James Doe",
-                Company = "BMW",
-                Model = "M5",
-                Color = "red"
-            });
-
-            addCarToPoliceDatabase(factory, new Car
-            {
-                Number = "CL234IR",
-                Owner = "James Doe",
-                Company = "BMW",
-                Model = "X1",
-                Color = "red"
-            });
-
-            factory.listFlyweights();
+            IUserOrder userOrder = new UserOrder();
+            Console.WriteLine("Facade : Start");
+            Console.WriteLine("************************************");
+            int cartID = userOrder.AddToCart(10, 1);
+            int userID = 1234;
+            Console.WriteLine("************************************");
+            int orderID = userOrder.PlaceOrder(cartID, userID);
+            Console.WriteLine("************************************");
+            Console.WriteLine("Facade : End CartID = {0}, OrderID = {1}",
+                cartID, orderID);
+            Console.ReadLine();
 
 
             Console.ReadKey(true);
-        }
-
-
-        public static void addCarToPoliceDatabase(FlyweightFactory factory, Car car)
-        {
-            Console.WriteLine("\nClient: Adding a car to database.");
-
-            var flyweight = factory.GetFlyweight(new Car
-            {
-                Color = car.Color,
-                Model = car.Model,
-                Company = car.Company
-            });
-            flyweight.Operation(car);
         }
     }
 }
